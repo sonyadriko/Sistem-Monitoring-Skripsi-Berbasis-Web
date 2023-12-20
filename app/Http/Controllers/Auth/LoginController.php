7031b9aa07922269c1b44ed94422ad93b5442ad5
Akbar Bintang M <akbarbintangmahendra@gmail.com>
2023-12-20 22:09:44 +0700

    Fix: Login Method

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -10,40 +10,12 @@ use Illuminate\Support\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use App\Models\User as MUser;
+use App\Models\User;
 
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
-    // use AuthenticatesUsers;
-
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
     protected $redirectTo = RouteServiceProvider::HOME;
-
-    // /**
-    //  * Create a new controller instance.
-    //  *
-    //  * @return void
-    //  */
-    // public function __construct()
-    // {
-    //     $this->middleware('guest')->except('logout');
-    // }
 
     public function showLoginForm()
     {
@@ -52,65 +24,63 @@ class LoginController extends Controller
 
     public function login(Request $request)
     {
-        $user = MUser::where('email', $request->email)->first();
-
-        if ($user === null) {
-            return 'Email tidak ditemukan';
-        }
-
-        $credentials = [
-            'email' => $request->email,
-            'password' => $request->password
-        ];
-
-        $role = null;
-
-        if ($user->role_id == 1) {
-            $role = 'mahasiswa';
-        } elseif ($user->role_id == 2) {
-            $role = 'dosen';
-        } elseif ($user->role_id == 3) {
-            $role = 'koordinator';
-        } elseif ($user->role_id == 4) {
-            $role = 'ketuajurusan';
+        $remember = $request->has('remember') ? true : false;
+        $credentials = $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+        $email = DB::table('users')
+            ->where('email', $credentials['email'])
+            ->pluck('email')
+            ->first();
+        $password = DB::table('users')
+            ->where('email', $credentials['email'])
+            ->pluck('password')
+            ->first();
+        $passwordCheck = Hash::check($request->password, $password);
+        if ($credentials['email'] != $email) {
+            return redirect()
+                ->route('auth-login')
+                ->with('danger', 'Your Email is wrong.');
         } else {
-            return 'Role tidak diizinkan';
-        }
-
-        if ($role !== null) {
-            // Pengguna memiliki peran yang sesuai dengan guards yang didefinisikan
-            $login = \Auth::guard($role)->attempt($credentials);
-        } else {
-            return 'Role tidak diizinkan';
-        }
-
-        if ($login) {
-            // Authentication successful
-            if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
-                DB::table('histories')->insert([
-                    'name_history' => 'Login', // Nama Aksi
-                    'content_history' => 'Login berhasil', // Isi deskripsi dari aksi
-                    'alert_history' => 'success', // kalau berhasil, maka success, kalau gagal maka warning atau danger (BS4)
-                    'action_history' => 'user-add', // icon, tergantung dari pakainya apa
-                    'status' => 1,
-                    'created_by' => Auth::user()->id,
-                    'created_at' => Carbon::now(),
-                    'updated_by' => Auth::user()->id,
-                    'updated_at' => Carbon::now(),
-                ]);
+            if ($passwordCheck) {
+                if (Auth::attempt(['email' => $request->input('email'), 'password' => $request->input('password')], $remember) || Auth::attempt($credentials)) {
+                    $request->session()->regenerate();
+                    DB::table('histories')->insert([
+                        'name_history' => 'Login',
+                        'content_history' => 'Login berhasil',
+                        'alert_history' => 'success',
+                        'action_history' => 'user-check',
+                        'status' => 1,
+                        'created_by' => Auth::user()->id,
+                        'created_at' => Carbon::now(),
+                        'updated_by' => Auth::user()->id,
+                        'updated_at' => Carbon::now(),
+                    ]);
+                    $getRoleID = Auth::user()->role_id;
+                    if ($getRoleID == '1') {
+                        return redirect()->route('home');
+                    } else if ($getRoleID == '2') {
+                        return redirect()->route('dashboard:dosen');
+                    } else if ($getRoleID == '3') {
+                        return redirect()->route('dashboard:koordinator');
+                    } else if ($getRoleID == '4') {
+                        return redirect()->route('dashboard:ketuajurusan');
+                    } else {
+                        return redirect()
+                            ->route('auth-login')
+                            ->with('danger', 'Maaf, mungkin Email atau Password Anda salah, mohon ulangi sekali lagi.');
+                    }
+                } else {
+                    return redirect()
+                        ->route('auth-login')
+                        ->with('danger', 'Maaf, mungkin Email atau Password Anda salah, mohon ulangi sekali lagi.');
+                }
+            } else {
+                return redirect()
+                    ->route('auth-login')
+                    ->with('danger', 'Your Password is wrong.');
             }
-            if ($role === 'koordinator') {
-                return redirect('/koordinator');
-            } elseif ($role === 'dosen') {
-                return redirect('/dosen');
-            } elseif ($role == 'mahasiswa') {
-                return redirect('/dashboard'); // Atau sesuaikan dengan halaman dashboard yang sesuai dengan peran
-            } elseif ($role = 'ketuajurusan') {
-                return redirect('/ketua_jurusan');
-            }
-        } else {
-            // Authentication failed
-            return redirect('/login')->with('error', 'Invalid email or password. Please try again.');
         }
     }
 
@@ -134,31 +104,4 @@ class LoginController extends Controller
         auth()->guard('ketuajurusan')->logout();
         return redirect('/login');
     }
-
-
-
-
-    // public function logout(Request $request)
-    // {
-    //     if (! Auth::user()) {
-    //         return redirect()
-    //             ->route('auth-login')
-    //             ->with('success', 'Terima kasih telah menggunakan aplikasi ini!');
-    //     } else {
-    //         if ($request->ajax()) {
-    //             Auth::Logout();
-
-    //             return redirect()
-    //                 ->route('auth-login')
-    //                 ->with('success', 'Mohon Login dengan password baru Anda.');
-    //         } else {
-    //             Auth::Logout();
-
-    //             return redirect()
-    //                 ->route('auth-login')
-    //                 ->with('success', 'Terima kasih telah menggunakan aplikasi ini!');
-    //         }
-    //     }
-    // }
-
 }
--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,28 +12,32 @@ class Authenticate extends Middleware
      * Get the path the user should be redirected to when they are not authenticated.
      */
 
-    public function handle($request, Closure $next, ... $guards)
-    {
-        $this->guards = $guards;
-        $this->authenticate($request,$guards);
+    // public function handle($request, Closure $next, ... $guards)
+    // {
+    //     $this->guards = $guards;
+    //     $this->authenticate($request,$guards);
 
-        return $next($request);
-    }
+    //     return $next($request);
+    // }
+
     protected function redirectTo($request)
     {
-        if ($request->expectsJson()) {
-            return null;
+        // if ($request->expectsJson()) {
+        //     return null;
+        // }
+        // foreach ($this->guards as $guard) {
+        //     if (in_array($guard, ['mahasiswa', 'dosen', 'koordinator', 'ketuajurusan'])) {
+        //         if (!auth()->guard($guard)->check()) {
+        //             return redirect()->route('auth-login');  // Mengarahkan ke 'login' jika tidak terotentikasi
+        //         } else {
+        //             return redirect()->route('dashboard');  // Mengarahkan ke 'dashboard' jika terotentikasi
+        //         }
+        //     }
+        // }
+        // // Tidak ada peran yang sesuai, arahkan ke 'login'
+        // return redirect()->route('auth-login');
+        if (! $request->expectsJson()) {
+            return redirect()->route('auth-login');
         }
-        foreach ($this->guards as $guard) {
-            if (in_array($guard, ['mahasiswa', 'dosen', 'koordinator', 'ketuajurusan'])) {
-                if (!auth()->guard($guard)->check()) {
-                    return route('login');  // Mengarahkan ke 'login' jika tidak terotentikasi
-                } else {
-                    return route('dashboard');  // Mengarahkan ke 'dashboard' jika terotentikasi
-                }
-            }
-        }
-        // Tidak ada peran yang sesuai, arahkan ke 'login'
-        return route('login');
     }
 }
