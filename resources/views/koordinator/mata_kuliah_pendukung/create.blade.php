0bca014090be2ea7971aab9a3fadaaf0b3416cfd
renataalexandria <xnightsky2008@gmail.com>
2024-05-13 22:48:51 +0700

    recode mata kuliah pendukung user koordinator

--- resources/views/koordinator/mata_kuliah_pendukung/create.blade.php
+++ resources/views/koordinator/mata_kuliah_pendukung/create.blade.php
@@ -1,114 +1,113 @@
 @extends('layout.master')
 
-@section('title')
-Tambah Mata Kuliah Pendukung
-@endsection
+@section('title', 'Tambah Mata Kuliah Pendukung')
 
 @section('css')
-<link href="{{ asset('assets2/libs/datatables.net-bs4/datatables.net-bs4.min.css') }}" rel="stylesheet" type="text/css" />
-<link href="{{ asset('assets2/libs/datatables.net-buttons-bs4/datatables.net-buttons-bs4.min.css') }}" rel="stylesheet" type="text/css" />
-<link href="{{ asset('assets2/libs/datatables.net-responsive-bs4/datatables.net-responsive-bs4.min.css') }}" rel="stylesheet" type="text/css" />
-<!-- Include Font Awesome for icons -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha384-GLhlTQ8iN17EtM8Ckdo3hxJY98p+KBg0qD/I9z2QFmW6qq5CIK5BA/1EL86W39SK" crossorigin="anonymous">
-
+    <link href="{{ asset('assets2/libs/datatables.net-bs4/datatables.net-bs4.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets2/libs/datatables.net-buttons-bs4/datatables.net-buttons-bs4.min.css') }}" rel="stylesheet"
+        type="text/css" />
+    <link href="{{ asset('assets2/libs/datatables.net-responsive-bs4/datatables.net-responsive-bs4.min.css') }}"
+        rel="stylesheet" type="text/css" />
+    <!-- Include Font Awesome for icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+        integrity="sha384-GLhlTQ8iN17EtM8Ckdo3hxJY98p+KBg0qD/I9z2QFmW6qq5CIK5BA/1EL86W39SK" crossorigin="anonymous">
 @endsection
 @section('content')
-<nav class="page-breadcrumb">
-    <ol class="breadcrumb">
-      <li class="breadcrumb-item"><a href="#">Manajemen</a></li>
-      <li class="breadcrumb-item active" aria-current="page">Mata Kuliah Pendukung</li>
-    </ol>
-</nav>
-<div class="row">
-    <div class="col-md-12 stretch-card">
-        <div class="card mb-4">
-            <h5 class="card-header">Mata Kuliah Pendukung</h5>
-            <div class="card-body">
-                <form action="{{route('mata-kuliah.store')}}" method="POST"enctype="multipart/form-data" id="mataKuliahForm">
-                    @csrf
-                    <div class="row">
-                        <div class="mb-3">
-                            <label for="nama_mata_kuliah" class="form-label">Nama Mata Kuliah</label>
-                            <input type="text" class="form-control" id="nama_mata_kuliah" name="nama_mata_kuliah" placeholder="Masukan Mata Kuliah..." aria-describedby="defaultFormControlHelp" />
+    <nav class="page-breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="#">Manajemen</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Mata Kuliah Pendukung</li>
+        </ol>
+    </nav>
+    <div class="row">
+        <div class="col-md-12 stretch-card">
+            <div class="card mb-4">
+                <h5 class="card-header">Mata Kuliah Pendukung</h5>
+                <div class="card-body">
+                    <form action="{{ route('mata-kuliah.store') }}" method="POST"enctype="multipart/form-data"
+                        id="mataKuliahForm">
+                        @csrf
+                        <div class="row">
+                            <div class="mb-3">
+                                <label for="nama_mata_kuliah" class="form-label">Nama Mata Kuliah</label>
+                                <input type="text" class="form-control" id="nama_mata_kuliah" name="nama_mata_kuliah"
+                                    placeholder="Masukan Mata Kuliah..." aria-describedby="defaultFormControlHelp" />
+                            </div>
+                            @error('nama_mata_kuliah')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
                         </div>
-                        @error('nama_mata_kuliah')
-                            <div class="invalid-feedback">{{ $message }}</div>
-                        @enderror
-                    </div>
-                    <div class="row">
-                        <div class="d-flex justify-content-between mt-4">
-                            <button type="button" class="btn btn-secondary" onclick="window.history.back();">Kembali</button>
-                            {{-- <button type="submit" class="btn btn-primary" onclick="submitForm()">Submit</button> --}}
-                            <button type="submit" class="btn btn-primary" onclick="$('#submitBtn').click();">Submit</button>
-
+                        <div class="row">
+                            <div class="d-flex justify-content-between mt-4">
+                                <button type="button" class="btn btn-secondary"
+                                    onclick="window.history.back();">Kembali</button>
+                                <button type="submit" class="btn btn-primary"
+                                    onclick="$('#submitBtn').click();">Submit</button>
+                            </div>
                         </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
-
 @endsection
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 
 
 
 @push('plugin-scripts')
-  <script src="{{ asset('assets/plugins/flatpickr/flatpickr.min.js') }}"></script>
-  <script src="{{ asset('assets/plugins/apexcharts/apexcharts.min.js') }}"></script>
-  <script src="{{ asset('assets/plugins/dropify/js/dropify.min.js') }}"></script>
-
+    <script src="{{ asset('assets/plugins/flatpickr/flatpickr.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/apexcharts/apexcharts.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/dropify/js/dropify.min.js') }}"></script>
 @endpush
 
 @push('custom-scripts')
-  <script src="{{ asset('assets/js/dashboard.js') }}"></script>
-  <script src="{{ asset('assets/js/dropify.js') }}"></script>
+    <script src="{{ asset('assets/js/dashboard.js') }}"></script>
+    <script src="{{ asset('assets/js/dropify.js') }}"></script>
 @endpush
 <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script>
-    $(document).ready(function () {
-    // Attach a click event to the submit button
-    $('#submitBtn').click(function (event) {
-        // Prevent the default form submission
-        event.preventDefault();
+    $(document).ready(function() {
+        // Attach a click event to the submit button
+        $('#submitBtn').click(function(event) {
+            // Prevent the default form submission
+            event.preventDefault();
 
-        // Perform the AJAX request
-        $.ajax({
-            url: "{{ route('mata-kuliah.store') }}",
-            method: "POST",
-            data: $('#mataKuliahForm').serialize(),
-            success: function (data) {
-                if (data.success) {
-                    Swal.fire({
-                        icon: 'success',
-                        title: 'Success!',
-                        text: data.message,
-                        showConfirmButton: false,
-                        timer: 1500
-                    }).then(function () {
-                        // Redirect after success
-                        window.location.href = "{{ route('bimbingan-mhs.index') }}";
-                    });
-                } else {
+            // Perform the AJAX request
+            $.ajax({
+                url: "{{ route('mata-kuliah.store') }}",
+                method: "POST",
+                data: $('#mataKuliahForm').serialize(),
+                success: function(data) {
+                    if (data.success) {
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Success!',
+                            text: data.message,
+                            showConfirmButton: false,
+                            timer: 1500
+                        }).then(function() {
+                            // Redirect after success
+                            window.location.href =
+                                "{{ route('bimbingan-mhs.index') }}";
+                        });
+                    } else {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Error!',
+                            text: data.message
+                        });
+                    }
+                },
+                error: function() {
                     Swal.fire({
                         icon: 'error',
                         title: 'Error!',
-                        text: data.message
+                        text: 'An error occurred while processing your request.'
                     });
                 }
-            },
-            error: function () {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Error!',
-                    text: 'An error occurred while processing your request.'
-                });
-            }
+            });
         });
     });
-});
-
 </script>
-
--- resources/views/koordinator/mata_kuliah_pendukung/index.blade.php
+++ resources/views/koordinator/mata_kuliah_pendukung/index.blade.php
@@ -1,92 +1,81 @@
 @extends('layout.master3')
 
-@section('title')
-Mata Kuliah Pendukung
-@endsection
+@section('title', 'Mata Kuliah Pendukung')
 
 @section('css')
-<link href="{{ asset('assets2/libs/datatables.net-bs4/datatables.net-bs4.min.css') }}" rel="stylesheet" type="text/css" />
-<link href="{{ asset('assets2/libs/datatables.net-buttons-bs4/datatables.net-buttons-bs4.min.css') }}" rel="stylesheet" type="text/css" />
-<link href="{{ asset('assets2/libs/datatables.net-responsive-bs4/datatables.net-responsive-bs4.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets2/libs/datatables.net-bs4/datatables.net-bs4.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets2/libs/datatables.net-buttons-bs4/datatables.net-buttons-bs4.min.css') }}" rel="stylesheet"
+        type="text/css" />
+    <link href="{{ asset('assets2/libs/datatables.net-responsive-bs4/datatables.net-responsive-bs4.min.css') }}"
+        rel="stylesheet" type="text/css" />
 
 @endsection
 @section('content')
-@if(session('success'))
-<div class="alert alert-success">
-    {{ session('success') }}
-</div>
-@endif
-<nav class="page-breadcrumb">
-    <ol class="breadcrumb">
-      <li class="breadcrumb-item"><a href="#">Manajemen</a></li>
-      <li class="breadcrumb-item active" aria-current="page">Mata Kuliah Pendukung</li>
-    </ol>
-</nav>
-<div class="row">
-    <div class="col-md-12 stretch-card">
-        <div class="card">
-            <div class="card-header d-flex justify-content-between">
-                <div class="text-left">
-                    <h5 class="card-title" style="font-weight: bold">Tabel List Mata Kuliah Pendukung</h5>
-                    <p class="card-title-desc">Berikut merupakan tabel list Mata Kuliah Pendukung.</p>
-                </div>
-                <div class="text-right">
-                    <a href="{{ url('/koordinator/mata_kuliah_pendukung/create') }}" class="btn btn-primary">Tambah Mata Kuliah</a>
-                </div>
-            </div>
-
-            <div class="card-body table-responsive">
-                <table id="datatable" class="table table-bordered dt-responsive  nowrap w-100">
-                    <thead>
-                    <tr>
-                        <th>No</th>
-                        <th>Nama Mata Kuliah</th>
-                        {{-- <th>Keterangan</th> --}}
-                        {{-- <th>Action</th> --}}
-                    </tr>
-                    </thead>
-                    <tbody>
-                        @php
-                    $no=1;
-                    @endphp
-                    @foreach($mk as $mk)
-                    <tr>
-                        <td>{{ $no }}</td>
-                        <td>{{ $mk->nama_mata_kuliah }}</td>
-                        {{-- <td>{{ $dosen->keterangan }}</td> --}}
-                        {{-- <td><a href="{{ url('/koordinator/mata_kuliah_pendukung/detail/' . $mk->id_mata_kuliah_pendukung) }}" class="btn btn-primary">Detail</a></td> --}}
-                    </tr>
-                    @php
-                    $no++;
-                    @endphp
-                    @endforeach
-                    </tbody>
-                </table>
-
-            </div>
+    @if (session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
         </div>
-    </div> <!-- end col -->
-</div> <!-- end row -->
+    @endif
+    <nav class="page-breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="#">Manajemen</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Mata Kuliah Pendukung</li>
+        </ol>
+    </nav>
+    <div class="row">
+        <div class="col-md-12 stretch-card">
+            <div class="card">
+                <div class="card-header d-flex justify-content-between">
+                    <div class="text-left">
+                        <h5 class="card-title" style="font-weight:bold">Tabel List Mata Kuliah Pendukung</h5>
+                        <p class="card-title-desc">Berikut merupakan tabel list Mata Kuliah Pendukung.</p>
+                    </div>
+                    <div class="text-right">
+                        <a href="{{ url('/koordinator/mata_kuliah_pendukung/create') }}" class="btn btn-primary">Tambah Mata
+                            Kuliah</a>
+                    </div>
+                </div>
 
+                <div class="card-body table-responsive">
+                    <table id="datatable" class="table table-bordered dt-responsive nowrap w-100">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Nama Mata Kuliah</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($mk as $index => $mataKuliah)
+                                <tr>
+                                    <td>{{ $index + 1 }}</td>
+                                    <td>{{ $mataKuliah->nama_mata_kuliah }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div> <!-- end col -->
+    </div> <!-- end row -->
 @endsection
 @push('plugin-scripts')
-  <script src="{{ asset('assets/plugins/flatpickr/flatpickr.min.js') }}"></script>
-  <script src="{{ asset('assets/plugins/apexcharts/apexcharts.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/flatpickr/flatpickr.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/apexcharts/apexcharts.min.js') }}"></script>
 @endpush
 
 @push('custom-scripts')
-  <script src="{{ asset('assets/js/dashboard.js') }}"></script>
+    <script src="{{ asset('assets/js/dashboard.js') }}"></script>
 @endpush
 
 @section('script')
-<script src="{{ asset('assets2/libs/datatables.net/datatables.net.min.js') }}"></script>
-<script src="{{ asset('assets2/libs/datatables.net-bs4/datatables.net-bs4.min.js') }}"></script>
-<script src="{{ asset('assets2/libs/datatables.net-buttons/datatables.net-buttons.min.js') }}"></script>
-<script src="{{ asset('assets2/libs/datatables.net-buttons-bs4/datatables.net-buttons-bs4.min.js') }}"></script>
-<script src="{{ asset('assets2/libs/jszip/jszip.min.js') }}"></script>
-<script src="{{ asset('assets2/libs/pdfmake/pdfmake.min.js') }}"></script>
-<script src="{{ asset('assets2/libs/datatables.net-responsive/datatables.net-responsive.min.js') }}"></script>
-<script src="{{ asset('assets2/libs/datatables.net-responsive-bs4/datatables.net-responsive-bs4.min.js') }}"></script>
-<script src="{{ asset('assets2/js/pages/datatables.init.js') }}"></script>
-<script src="{{ asset('assets2/js/app.min.js') }}"></script>
+    <script src="{{ asset('assets2/libs/datatables.net/datatables.net.min.js') }}"></script>
+    <script src="{{ asset('assets2/libs/datatables.net-bs4/datatables.net-bs4.min.js') }}"></script>
+    <script src="{{ asset('assets2/libs/datatables.net-buttons/datatables.net-buttons.min.js') }}"></script>
+    <script src="{{ asset('assets2/libs/datatables.net-buttons-bs4/datatables.net-buttons-bs4.min.js') }}"></script>
+    <script src="{{ asset('assets2/libs/jszip/jszip.min.js') }}"></script>
+    <script src="{{ asset('assets2/libs/pdfmake/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('assets2/libs/datatables.net-responsive/datatables.net-responsive.min.js') }}"></script>
+    <script src="{{ asset('assets2/libs/datatables.net-responsive-bs4/datatables.net-responsive-bs4.min.js') }}"></script>
+    <script src="{{ asset('assets2/js/pages/datatables.init.js') }}"></script>
+    <script src="{{ asset('assets2/js/app.min.js') }}"></script>
 @endsection
